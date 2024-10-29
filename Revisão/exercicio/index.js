40637886088f3ec66b09dc6d15959b94f67e4fca
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-10-28 22:05:10 -0300

    .

---
+++ Revisão/exercicio/index.js
@@ -0,0 +1,16 @@
+function meuEscopo(){
+    const form = document.querySelector(".form");
+    const resultado = document.querySelector('resultado')
+
+    function eventoForm(evento){
+        evento.preventDefault()
+        const nome = form.querySelector(".nome");
+        const sobrenome = form.querySelector(".sobrenome");
+        const peso = form.querySelector(".peso");
+        const altura = form.querySelector(".altura");
+    };
+
+    form.addEventListener('submit',eventoForm)
+
+};
+meuEscopo()
