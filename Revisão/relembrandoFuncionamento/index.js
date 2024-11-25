83e28900c5eb0734b6fc1e2ce1310b46fff6416f
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-11-25 15:04:51 -0300

    Update index.js

--- Revisão/relembrandoFuncionamento/index.js
+++ Revisão/relembrandoFuncionamento/index.js
@@ -1,57 +1,57 @@
-let idade = 18;
+// let idade = 18;
 
-if (idade >= 18) {
-    // console.log("Maior de idade");
-} else {
-  //  console.log("Menor de idade")
-};
-
-
-
-for (let i = 0;i <= idade;i++) {
-   // console.log(i, idade)
-};
+// if (idade >= 18) {
+     // console.log("Maior de idade");
+// } else {
+   //  console.log("Menor de idade")
+// };
 
 
-j = 2;
-while (j < 100) {
-    j += 2
-   // console.log(j)
-};
 
-function soma(a, b) {
-    return a + b;
-};
+// for (let i = 0;i <= idade;i++) {
+    // console.log(i, idade)
+// };
 
-const subtracao = function(a, b) {
-    return a - b;
-};
 
-const multiplicacao = (a, b) => a * b;
+// j = 2;
+// while (j < 100) {
+//     j += 2
+    // console.log(j)
+// };
 
-const divisao = (a, b) => a / b;
+// function soma(a, b) {
+//     return a + b;
+// };
+
+// const subtracao = function(a, b) {
+//     return a - b;
+// };
+
+// const multiplicacao = (a, b) => a * b;
+
+// const divisao = (a, b) => a / b;
 
 // console.log(soma(1, 1));
 // console.log(multiplicacao(2, 2));
 // console.log(divisao(320, 20));
 // console.log(subtracao(3, 2));
 
-let carro = {
-    marca: "Toyota",
-    nome: 'Corolla',
-    ano: 2020,
-    ligar: function(){
-        console.log("Carro ligado")
-    }
-};
+// let carro = {
+//     marca: "Toyota",
+//     nome: 'Corolla',
+//     ano: 2020,
+//     ligar: function(){
+//         console.log("Carro ligado")
+//     }
+// };
 
 // console.log(carro.marca, carro.nome, carro.ano)
 // carro.ligar()
 
-let numeros = [1,2,3,4,5,6,7,8,9]
-numeros.pop()
-numeros.push(10)
+// let numeros = [1,2,3,4,5,6,7,8,9]
+// numeros.pop()
+// numeros.push(10)
 
-for (num in numeros) {
-    console.log(num, numeros[num])
-}
+// for (num in numeros) {
+//     console.log(num, numeros[num])
+// }
