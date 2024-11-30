618f9b09566f117714d396c7573e62532602f8e8
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-11-30 06:00:52 -0300

    .

--- Revisão/2_exercicio/assets/js/index.js
+++ Revisão/2_exercicio/assets/js/index.js
@@ -1,3 +1,21 @@
 form = document.querySelector('.form');
-peso = form.querySelector(".peso")
 console.log()
+
+
+function setResultado(msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado')
+    p.innerHTML = 'qwertgh'
+    resultado.appendChild(p)
+    
+};
+
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log('Evento prevenido');
+    setResultado("agb");
+});
