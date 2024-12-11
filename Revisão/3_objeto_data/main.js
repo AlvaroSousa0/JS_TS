1f8242693a19e63669f7804a2a2ef561c792b51e
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-12-11 09:11:53 -0300

    ...

--- Revisão/3_objeto_data/main.js
+++ Revisão/3_objeto_data/main.js
@@ -1,2 +1,33 @@
-const data = new Date('2024-12-10 14:23:43')
+const data = new Date('2024-11-25')
 console.group(data.toString())
+
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch(diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6: 
+        diaSemanaTexto = 'Sábado';
+        break;
+    default:
+        diaSemanaTexto = 'Dia Inválido'
+}
+
+console.log(diaSemana, diaSemanaTexto)
