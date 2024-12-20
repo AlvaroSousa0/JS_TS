9090c7d2fa7465364cb00f8c95913d068de4129f
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-12-20 11:12:39 -0300

    exercicio de timer browser

---
+++ Revisão/9_exercicio_timer/assets/js/index.js
@@ -0,0 +1,4 @@
+const timer = document.querySelector('.timer');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
