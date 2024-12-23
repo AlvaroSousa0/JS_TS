13bd45dd998e5f6bb9f0300f036e35f02289b7f3
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-12-23 09:02:20 -0300

    timer bacaninha

--- Revisão/9_exercicio_timer/assets/js/index.js
+++ Revisão/9_exercicio_timer/assets/js/index.js
@@ -1,4 +1,42 @@
 const timer = document.querySelector('.timer');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+let counter;
+let segundos = 0;
+
+
+
+function criaRelogio(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+ function iniciaRelogio() {
+    counter = setInterval(function() {
+        segundos++;
+        timer.innerHTML = criaRelogio(segundos);
+    }, 1000)
+ }
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('zerar')) {
+        clearInterval(counter);
+        timer.innerHTML = '00:00:00';
+        timer.classList.remove('pausado');
+        segundos = 0;
+    };
+
+    if (el.classList.contains('iniciar')) {
+        timer.classList.remove('pausado');
+        clearInterval(counter);
+        iniciaRelogio();
+    };
+
+    if (el.classList.contains('pausar')) {
+        clearInterval(counter);
+        timer.classList.add('pausado');
+    };
+})
+
