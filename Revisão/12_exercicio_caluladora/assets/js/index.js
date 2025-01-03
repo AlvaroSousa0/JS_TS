a6407e231775573ae8aec242813367ff40823182
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2025-01-03 11:39:27 -0300

    js finalizado, eu acho

--- Revisão/12_exercicio_caluladora/assets/js/index.js
+++ Revisão/12_exercicio_caluladora/assets/js/index.js
@@ -3,7 +3,28 @@ function criaCalculadora() {
         display: document.querySelector('.display'),
         inicia() {
             this.cliqueBotao();
+            this.btnBackSpace();
+            this.btnEnter();
+            this.display.focus();
+        },
 
+        delUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        clearDisplay() {
+            this.display.value = ''
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+            try{
+                conta = eval(conta);
+            
+                this.display.value = String(conta);
+            }catch (e){
+                alert('Conta Inválida');
+            };
         },
 
         cliqueBotao() {
@@ -13,13 +34,39 @@ function criaCalculadora() {
                 if (el.classList.contains('btn-num')) {
                     this.btnDisplay(el.innerText)
                 }
+                if (el.classList.contains('btn-del')) {
+                    this.delUm()
+                }
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay()
+                }
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta()
+                }
             })
         },
   
         btnDisplay(valor) {
             this.display.value += valor
         },
-  
+        
+        btnBackSpace() {
+            this.display.addEventListener('keydown', e => {
+                if (e.keyCode == 8) {
+                    e.preventDefault();
+                    this.delUm();
+                }
+            })
+        },
+
+        btnEnter() {
+            this.display.addEventListener('keyup', e => {                 
+                if (e.keyCode == 13) {
+                    this.realizaConta();
+                }
+            })
+        },
+
     }
 };
 
